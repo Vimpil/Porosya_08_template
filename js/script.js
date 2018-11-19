b591c2028cb5309020745803e363be224c3be11d
Vimpil <vimpil@ya.ru>
2018-11-20 00:29:47 +0300

    finished elements.html added navigation

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(function() {
+
    /* START Dropdown */
    var x, i, j, selElmnt, a, b, c;
    /*look for any elements with the class "custom-select":*/
@@ -75,7 +76,7 @@ $(function() {
    document.addEventListener("click", closeAllSelect);
    /* END Dropdown */
 
-   
+
    $("#add_file").mouseover(function(event) {
       $(this).parent().children('input').mouseover();
    });
@@ -110,7 +111,7 @@ $(function() {
    if ($(window).width() >= 799) {
       $('html').toggleClass('mm-wrapper_sidebar-expanded mm-wrapper_opened mm-wrapper_blocking mm-wrapper_background mm-wrapper_opening');
       $('#menu').toggleClass('mm-menu mm-menu_offcanvas mm-menu_sidebar-expanded mm-menu_opened mm-menu_widescreen');
-      $('html').toggleClass('mm-wrapper_opening');      
+      $('html').toggleClass('mm-wrapper_opening');
    }
    $(window).on('resize', function() {
       if ($(window).width() >= 799) {
@@ -151,6 +152,32 @@ window.onclick = function(e) {
    }
 }
 jQuery(document).ready(function($) {
+
+   // When the user scrolls down 20px from the top of the document, show the button
+   window.onscroll = function() {
+      scrollFunction()
+   };
+
+   function scrollFunction() {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+         document.getElementById("back_top_btn").style.display = "block";
+          // document.getElementById("call_to_us").style.display = "block";
+          //  document.getElementById("write_to_us").style.display = "block";
+      } else {
+         document.getElementById("back_top_btn").style.display = "none";
+         // document.getElementById("call_to_us").style.display = "none";
+         // document.getElementById("write_to_us").style.display = "none";
+
+      }
+   }
+
+   // When the user clicks on the button, scroll to the top of the document
+   $("#back_top_btn").click(function(event) {
+      /* Act on the event */
+      document.body.scrollTop = 0; // For Safari
+      document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+   });
+
    $(".accordion").click(function(event) {
       /* Act on the event */
       $(this).parent().children('.panel').slideToggle();
